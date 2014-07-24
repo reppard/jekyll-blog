d47f715e9d8786c715b30264ecaad02f627acaa4
reppard <reppardwalker@gmail.com>
2014-07-23 21:20:40 -0400

    Jquery for coloring color bars...I love jquery

---
+++ js/bar.js
@@ -0,0 +1,11 @@
+var darkColors = Array('#000000','#0000AA','#00AA00','#00AAAA','#AA0000','#AA00AA','#AA5500','#AAAAAA')
+
+var lightColors = Array('#555555','#5555FF','#55FF55','#55FFFF','#FF5555','#FF55FF','#FFFF55','#FFFFFF')
+
+for(i=0;i<8;i++){
+  $('.dark-color-bar').append('<div class="color-box" style="background-color:'+darkColors[i]+'"></div>');
+}
+
+for(i=0;i<8;i++){
+  $('.light-color-bar').append('<div class="color-box" style="background-color:'+lightColors[i]+'"></div>');
+}
